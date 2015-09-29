8bd04463fdfeaa9e31e5de5b03dc4e225ae2fcfd
Yujie <yujiezhang2008@gmail.com>
2015-09-29 12:06:26 +0200

    Init code

---
+++ ScrollViewExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ScrollViewExample
+//
+//  Created by Yujie Zhang on 28/09/15.
+//  Copyright © 2015 ZYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
